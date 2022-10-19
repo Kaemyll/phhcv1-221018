6cad8de75c5b69c54ba1d27af4cac4edd8755e19
Garnier David <david.garnier483@orange.fr>
2022-10-19 16:45:24 +0200

    dev: polishing styles of categories

--- include/pages/categorie/categories.php
+++ include/pages/categorie/categories.php
@@ -1,5 +1,8 @@
 <h2>Liste des catégories</h2>
 <hr />
+
+<div class="flex-container">
+
 <?php
 
 include_once 'include/functions/produits.functions.php';
@@ -8,17 +11,19 @@ $categories = getAllCategories();
 
 foreach ($categories as $categorie) {
     ?>
-    <div class="categories-titres">
-        <h3>ID</h3>
-        <h3>Nom de la catégorie</h3>
-        <h3>TVA (%)</h3>
-        <h3>Actions</h3>
-    </div>
-    <div class="categorie">
-        <div class="categorie-id"><?=$categorie['id']?></div>
-        <div class="categorie-nom"><?=$categorie['nom']?></div>
-        <div class="categorie-tva"><?=$categorie['tva']?></div>
-        <div class="categorie-bouton"><a href="?page=produits&id_categories=<?=$categorie['id']?>"><button type="button" class="btn btn-primary">voir</button></a></div>
-    </div>
-    <?php
-}
+
+        <div class="flex-category">
+            <div class="category-id">ID : <?=$categorie['id']?></div>
+            <div class="category-nom"><?=$categorie['nom']?></div>
+            <div class="categorie-tva">TVA : <?=$categorie['tva']?>%</div>
+            <div class="categorie-bouton"><a href="?page=produits&id_categories=<?=$categorie['id']?>"><button type="button" class="btn btn-primary">voir</button></a></div>
+        </div>
+
+        <?php
+}?>
+
+</div>
+
+
+
+
